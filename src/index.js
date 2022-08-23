4461cd975e311381c169df3689ed623d9145a09a
charlie-s1 <charlie.d.smith98@gmail.com>
2022-08-23 16:21:10 +0100

    remove complete random Gradient add share option

--- src/index.js
+++ src/index.js
@@ -15,6 +15,26 @@ function RenderBox(props){
         ></div>
     )
 }
+function RenderDoneScreen(props){
+    
+    const date=new Date();
+    return(
+        <div id="doneScreenCont" style={{display:props.show ? "flex" : "none"}}>
+            <div id="doneText">
+                <h1>{date.toDateString()} Puzzle</h1>
+                <p>Completed in {props.turns} turns</p>
+                <button id="share" onClick={(e)=>copyToClipboard(e,date.toDateString(),props.turns)}>Share</button>
+            </div>
+        </div>
+    )
+}
+function copyToClipboard(e,date,turns){
+    navigator.clipboard.writeText(`I completed the puzzle on ${date} in ${turns} turns \nTry at charlie-s.com/gradientGame`).then(()=> {
+
+    },() => {
+        alert("ERROR")
+    })
+}
 function arrayEqual(a,b){
     if(a.length != b.length){
         return false;
@@ -40,19 +60,43 @@ class Board extends React.Component{
         }
     }
     newTurn(e,id){
-        let newUGrad = this.state.userGradient;
+        let newUGrad = this.state.userGradient.slice();
         newUGrad[id] = this.state.randomGradient[this.state.turn];
         let newTurn = this.state.turnsTaken+1;
-        this.setState({
-            turn:this.state.randomGradient.length-1<=this.state.turn? 0:this.state.turn+=1,
-            userGradient:newUGrad,
-            turnsTaken:newTurn,
-        })
+        let newRandomG = this.state.randomGradient.slice();
+        newRandomG.map((g)=>{
+            newUGrad.map((n)=>{
+                if(g.col==n.col && n.correct){
+                    g.correct = true;
+                }
+            });
+        });
+
+        if(!this.state.done){
+            if(this.state.turn==newRandomG.length-1 && newRandomG[0].correct){
+                this.state.turn=0;
+            }
+            if(newRandomG[this.state.turn+1] && newRandomG[this.state.turn+1].correct){
+                for (let i=this.state.turn;i<newRandomG.length;i++){
+                    if(newRandomG[this.state.turn+1] && newRandomG[this.state.turn+1].correct){
+                        this.state.turn++;
+                    }
+                }
+            }
+        }
         if(arrayEqual(this.state.gradient,this.state.userGradient)){
+            console.log("done");
             this.setState({
                 done:true,
             })            
         }
+        this.setState({
+            randomGradient:newRandomG,
+            turn:this.state.randomGradient.length-1<=this.state.turn? 0:this.state.turn+=1,
+            userGradient:newUGrad,
+            turnsTaken:newTurn,
+        });
+        
     }
 
     randomColour(addition){
@@ -73,14 +117,14 @@ class Board extends React.Component{
         const newGradient = new Gradient()
             .setColorGradient(`#${this.randomColour(0)}`,`#${this.randomColour(1)}`)
             .getColors();
-        this.state.gradient=newGradient.map(i=>{return {col:i,done:false}});
-        this.state.userGradient=Array(newGradient.length).fill(null)
+        this.state.gradient=newGradient.map(i=>{return {col:i,correct:false}});
+        this.state.userGradient=Array(newGradient.length).fill({});
     }
     createGBoxes(){
         let toReturn = [];
-        let count=0;
         if(this.state.done){
             this.createGradient();
+
             let randomG = this.state.gradient.slice();
             let currentIndex = randomG.length,  randomIndex;
             while (currentIndex != 0) {
@@ -92,19 +136,18 @@ class Board extends React.Component{
             }
             this.state.randomGradient = randomG;
         }
-        this.state.done = false;
-        for(const g of this.state.randomGradient){
+        this.state.done = arrayEqual(this.state.userGradient,this.state.gradient)
+        const rGradient = this.state.randomGradient.slice();
+        rGradient.map((n,index)=>{
             toReturn.push(
                 <RenderBox
-                    key={"0"+this.state.gradient.indexOf(g)}
-                    selected={count==this.state.turn?true:false}
-                    done={g.done}
-                    col={g.col}
-                    correct={null}
+                    key={"0"+index}
+                    selected={index==this.state.turn?true:false}
+                    col={n.col}
+                    correct={n.correct}
                 />
             )
-            count++;
-        }
+        })
         return toReturn;
     }
     createUBoxes(){
@@ -112,7 +155,7 @@ class Board extends React.Component{
         let count=0;
         for(const box of this.state.userGradient){
             if(this.state.gradient[count]==this.state.userGradient[count]){
-                this.state.randomGradient[this.state.randomGradient.indexOf(this.state.gradient[count])];
+                this.state.userGradient[count].correct = true;
             }
             newBoxes.push(
                 <RenderBox
@@ -136,6 +179,10 @@ class Board extends React.Component{
                 <aside className="colourCont" id="userG">
                     {this.createUBoxes()}
                 </aside>
+                <RenderDoneScreen
+                    show={this.state.done}
+                    turns={this.state.turn}
+                />
                 <p id="turnCount">{this.state.turnsTaken}</p>
             </div>
         )
