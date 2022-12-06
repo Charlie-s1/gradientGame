be429f9d77bbc6b12d054c31d3b57ef07a708a61
charlie-s1 <charlie.d.smith98@gmail.com>
2022-12-06 14:44:55 +0000

    revert config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ module.exports = {
     entry: path.join(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, "build"),
-        publicPath: "/"
     },
     mode: 'development',
     module: {
