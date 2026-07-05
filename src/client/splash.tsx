b6dc9bb7719899388116dbabddedf8c44f3672b6
Charlie <charlie.d.smith98@gmail.com>
2026-07-05 16:08:27 +0100

    update splash ui

--- src/client/splash.tsx
+++ src/client/splash.tsx
@@ -29,29 +29,41 @@ export const Splash = ({
   }, []);
 
   return (
-    <div className="flex relative flex-col justify-center items-center min-h-screen gap-4 bg-white dark:bg-gray-900">
-      <div className="flex gap-2 items-center">
-        <img
-          className="object-contain w-1/2 max-w-[250px] mx-auto"
-          src={userData.avatarUrl}
-          alt="Snoo"
-        />
-        <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
-          Hey {userData.username ?? "user"}
-        </h1>
-        <div className="flex flex-col justify-between h-full">
-          <p className="text-base text-center text-gray-600 dark:text-gray-300">
-            Welcome back to GradientGame
-          </p>
-          <div>
-            <div className="flex justify-between text-base text-center text-gray-600 dark:text-gray-300">
-              <p>Streak:</p>
-              <p>{userData.gameData.streak || 0} 🔥</p>
+    <div className="flex relative flex-col justify-center items-center min-h-screen gap-4 bg-slate-50 text-slate-800 dark:bg-gray-950 dark:text-slate-100">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col gap-3">
+          <div className="">
+            <img
+              className="object-contain w-24 h-25 mx-auto"
+              src={userData.avatarUrl}
+              alt="Snoo"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <h1 className="text-2xl font-bold text-center text-gray-900 dark:text-white">
+              Hey {userData.username ?? "user"}
+            </h1>
+            <p className="text-xs uppercase text-center tracking-wider text-slate-500 dark:text-slate-500">
+              Welcome back to GradientGrid
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-between h-full bg-white border-slate-200 dark:border-slate-800/80 dark:bg-slate-900/50 rounded-2xl border w-full max-2-sm backdrop-blur-sm p-5">
+          <div className="flex flex-col gap-3 items-center">
+            <div className="flex items-center px-4 text-sm py-1.5 border-orange-500/20 border rounded-full bg-amber-500/10">
+              <p className="text-orange-400 font-bold uppercase">
+                Current Streak{" "}
+                <span className="text-slate-950 dark:text-white">
+                  {userData.gameData.streak || 0}
+                </span>
+                🔥
+              </p>
             </div>
             {hasCompletedToday(userData.gameData.completedPuzzleDate) && (
               <div>
-                <p className="text-base text-center text-gray-600 dark:text-gray-300">
-                  Result from today:
+                <p className="mb-3 text-[10px] font-bold tracking-widest text-slate-400 uppercase dark:text-slate-500">
+                  Today's Result
                 </p>
                 <div className="whitespace-pre-line leading-6 tracking-wide text-center">
                   {userData.gameData.dailyResult?.emojiGrid}
@@ -61,7 +73,7 @@ export const Splash = ({
           </div>
         </div>
       </div>
-      <div className="flex items-center justify-center mt-5">
+      <div className="flex items-center justify-center">
         <button
           className={` ${hasCompletedToday(userData.gameData.completedPuzzleDate) ? "opacity-50 cursor-default!" : "hover:bg-[#c23300] dark:hover:bg-orange-700"} flex items-center justify-center bg-[#d93900] dark:bg-orange-600 text-white w-auto h-10 rounded-full cursor-pointer transition-colors px-4`}
           onClick={() =>
@@ -113,7 +125,7 @@ const MainApp = () => {
 
   if (isLoading) {
     return (
-      <div className="flex h-screen items-center justify-center bg-gray-900 text-white">
+      <div className="flex h-screen items-center justify-center bg-slate-50 text-slate-800 dark:bg-gray-950 dark:text-slate-100">
         Loading Profile...
       </div>
     );
